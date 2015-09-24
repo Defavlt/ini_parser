291e24918e6a47f99a625c17465d8f6ca4ac4d46
Marcus Hansson <marcus.hansson@email.com>
2015-09-24 20:36:57 +0100

    Added customized GPL license message

--- include/ini_section.h
+++ include/ini_section.h
@@ -1,18 +1,25 @@
 /********************************************************************
-    This file is part of ini_parser.
 
-    ini_parser is free software: you can redistribute it and/or modify
+    ini_parser - a small ini parser in C++
+    Copyright (C) 2015 Marcus Hansson
+
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
-    ini_parser is distributed in the hope that it will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, see <http://www.gnu.org/licenses/>,
+    or write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     You should have received a copy of the GNU General Public License
-    along with ini_parser.  If not, see <http://www.gnu.org/licenses/>.
+    along with ini_parser.
+
 ********************************************************************/
 
 #ifndef _INI_SECTION_H
