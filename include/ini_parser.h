ba462bbfd85249478a54e071430b61d595da91ca
Marcus Hansson <marcus.hansson@email.com>
2015-09-24 20:26:24 +0100

    Added copyright

--- include/ini_parser.h
+++ include/ini_parser.h
@@ -14,6 +14,22 @@
     You should have received a copy of the GNU General Public License
     along with ini_parser.  If not, see <http://www.gnu.org/licenses/>.
 ********************************************************************/
+/********************************************************************
+    This file is part of ini_parser.
+
+    ini_parser is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ini_parser is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ini_parser.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************/
 
 #ifndef _INI_PARSER_H
 #define _INI_PARSER_H 1
